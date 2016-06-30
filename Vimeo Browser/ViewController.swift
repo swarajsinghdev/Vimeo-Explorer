dd424e364652eeba7621f7acd8a8aecc796c223b
Martin Kelly <marty.kelly2009@gmail.com>
2016-06-30 18:36:00 +0100

    Initial Commit

---
+++ Vimeo Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vimeo Browser
+//
+//  Created by Martin Kelly on 30/06/2016.
+//  Copyright © 2016 Martin Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
